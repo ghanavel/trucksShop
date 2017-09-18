9fbb41f5aad77565aef9efa5209c45ec694b054c
Asaithambi Ghanavel <ghanavel.asaithambi@volvo.com>
2017-09-18 21:38:08 +0530

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trucksShopApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the trucksShopApp
+ */
+angular.module('trucksShopApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name trucksShopApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the trucksShopApp
+ */
+angular.module('trucksShopApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
